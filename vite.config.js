301956e69c04b5453c57c67be7a0034c95184194
MosefSanogo <mosefsanogo366@gmail.com>
2025-06-07 15:47:18 +0200

    Quatre commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'build' 
+  base: '/Portfolio/', // IMPORTANT: remplacez par votre vrai nom de repo
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
