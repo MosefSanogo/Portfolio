3976940bb556f3bece49f9a403338794ceced1bb
MosefSanogo <mosefsanogo366@gmail.com>
2025-06-09 19:27:32 +0200

    Changement 5

---
+++ src/Methods/Data.js
@@ -0,0 +1,44 @@
+export const projects = [
+        {
+            title: "Logiciel web de gestion des élèves",
+            description: "Développement d’un logiciel de gestion des élèves et des professeurs pour une école, incluant la gestion des notes, des absences et des emplois du temps.",
+            image: "assets/Education.png",
+            technologies: ["Java", "Java EE", "JSP", "Servlets", "MySQL", "CSS", "HTML", "JavaScript", "Tomcat", "JSTL","Validation"],
+            link: ""
+        },
+        {
+            title: "Logiciel web de gestion de cabinet d'avocat",
+            description: "Création d’un logiciel de gestion pour un cabinet d’avocat, permettant la gestion des dossiers, des clients et des factures.",
+            image: "assets/Cabinet.png",
+            technologies: ["Java", "Java EE", "JSP", "Servlets", "MySQL", "CSS", "HTML", "JavaScript", "Tomcat", "JSTL","Web/Socket"],
+            link: "https://github.com/MosefSanogo/Cabinet/tree/master"
+        },
+        {
+            title: "Portfolio Personnel",
+            description: "Création d’un portfolio personnel pour présenter mes compétences et mes projets, incluant des sections sur mon parcours, mes compétences et mes réalisations.",
+            image: "assets/Portfolio.png",
+            technologies: ["ReactJs", "JSX", "GSAP", "CSS", "HTML", "JavaScript", "Vite", "React Router", "Hooks"],
+            link: "https://github.com/MosefSanogo/Portfolio"
+        },
+        {
+          title: "Billetterie",
+          description: "Développement d’un système de billetterie pour un événement, permettant la vente et la gestion des billets en ligne.",
+          image: "assets/Site.png",
+          technologies: ["ReactJs", "JSX", "GSAP", "CSS", "HTML", "JavaScript", "Vite", "React Router", "Hooks", "NodeJs", "Express", "MySQL", "JWT", "Axios" ,"Responsive Design", "Bycrypt"],
+          link: ""
+        },
+        {
+          title: "Login/Register",
+          description: "Développement d’un système de connexion et d’inscription sécurisé, permettant aux utilisateurs de créer un compte et de se connecter.",
+          image: "assets/Login.png",
+          technologies: ["ReactJs", "JSX", "CSS", "JavaScript", "Vite", "React Router", "Hooks", "NodeJs", "Express", "MySQL", "JWT", "Axios" ,"Responsive Design", "Bycrypt"],
+          link: ""
+        },
+        {
+          title: "Affichage des données de capteurs MQTT",
+          description: "Création d’une application web pour afficher les données de capteurs connectés via MQTT, permettant la visualisation en temps réel des données.",
+          image: "assets/Mqtt.png",
+          technologies: ["ReactJs", "JSX", "CSS", "JavaScript", "Vite", "React Router", "Hooks", "Axios" ,"MQTT", "WebSocket"],
+          link: ""
+        }
+    ];
--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,77 +1,17 @@
-import React, {  useEffect } from 'react'
+import React from 'react'
 import './project.css'
 import ProjetCard from '../../components/projet/ProjetCard'
-import gsap from 'gsap';
+import { useIntersecting } from '../../Methods/Intersecting';
+import { projects } from '../../Methods/Data';
 function Project({ref}) {
-    const projects = [
-        {
-            title: "Logiciel web de gestion des élèves",
-            description: "Développement d’un logiciel de gestion des élèves et des professeurs pour une école, incluant la gestion des notes, des absences et des emplois du temps.",
-            image: "assets/Education.png",
-            technologies: ["Java", "Java EE", "JSP", "Servlets", "MySQL", "CSS", "HTML", "JavaScript", "Tomcat", "JSTL","Validation"],
-            link: ""
-        },
-        {
-            title: "Logiciel web de gestion de cabinet d'avocat",
-            description: "Création d’un logiciel de gestion pour un cabinet d’avocat, permettant la gestion des dossiers, des clients et des factures.",
-            image: "assets/Cabinet.png",
-            technologies: ["Java", "Java EE", "JSP", "Servlets", "MySQL", "CSS", "HTML", "JavaScript", "Tomcat", "JSTL","Web/Socket"],
-            link: "https://github.com/MosefSanogo/Cabinet/tree/master"
-        },
-        {
-            title: "Portfolio Personnel",
-            description: "Création d’un portfolio personnel pour présenter mes compétences et mes projets, incluant des sections sur mon parcours, mes compétences et mes réalisations.",
-            image: "assets/Portfolio.png",
-            technologies: ["ReactJs", "JSX", "GSAP", "CSS", "HTML", "JavaScript", "Vite", "React Router", "Hooks"],
-            link: ""
-        },
-        {
-          title: "Billetterie",
-          description: "Développement d’un système de billetterie pour un événement, permettant la vente et la gestion des billets en ligne.",
-          image: "assets/Site.png",
-          technologies: ["ReactJs", "JSX", "GSAP", "CSS", "HTML", "JavaScript", "Vite", "React Router", "Hooks", "NodeJs", "Express", "MySQL", "JWT", "Axios" ,"Responsive Design", "Bycrypt"],
-          link: ""
-        },
-        {
-          title: "Login/Register",
-          description: "Développement d’un système de connexion et d’inscription sécurisé, permettant aux utilisateurs de créer un compte et de se connecter.",
-          image: "assets/Login.png",
-          technologies: ["ReactJs", "JSX", "CSS", "JavaScript", "Vite", "React Router", "Hooks", "NodeJs", "Express", "MySQL", "JWT", "Axios" ,"Responsive Design", "Bycrypt"],
-          link: ""
-        },
-        {
-          title: "Affichage des données de capteurs MQTT",
-          description: "Création d’une application web pour afficher les données de capteurs connectés via MQTT, permettant la visualisation en temps réel des données.",
-          image: "assets/Mqtt.png",
-          technologies: ["ReactJs", "JSX", "CSS", "JavaScript", "Vite", "React Router", "Hooks", "Axios" ,"MQTT", "WebSocket"],
-          link: ""
-        }
-    ];
-  const containRef = ref;
+    
   const h1Ref = React.useRef(null);
   const cardRef = React.useRef(null);
-  
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          const tl = gsap.timeline({ ease: "power2.out" });
-          tl.fromTo(h1Ref.current, { y: 80, opacity: 0 }, { y: 0, opacity: 1, duration: 1 })
-            .fromTo(cardRef.current, { y: 80, opacity: 0 }, { y: 0, opacity: 1, duration: 1 }, "-=0.8");
-          observer.unobserve(containRef.current);
-        }
-      },
-      { threshold: 0.2 }
-    );
 
-    if (containRef.current) {
-      observer.observe(containRef.current);
-    }
-
-    return () => observer.disconnect();
-  }, [containRef]);
+  useIntersecting(h1Ref, 0.2, 0.1);
+  useIntersecting(cardRef, 0.2, 0);
   return (
-    <div className='project' ref={containRef}>
+    <div className='project' ref={ref}>
       <h1 ref={h1Ref}>Projets</h1>
       <div className="project-list" ref={cardRef}>
         {projects.map((project, index) => (
